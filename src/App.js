ba7ea027e3af95d1fb21cf545734b50a99ce7a94
manish <manish@laptoplenovo>
2020-06-26 18:49:57 +0530

    simple counter via redux

--- src/App.js
+++ src/App.js
@@ -5,6 +5,82 @@ import './App.css';
 
 import { createStore } from 'redux'
 
+// our application state
+// this is a very simple counter application
+let init_state = 0
+
+
+// our actions
+// a) to increment counter
+// b) to decrement counter
+// type can be anything. generally "type" is used as default but it can any key you want.
+let increment_action = {
+  "type" : "increment"
+}
+let decrement_action = {
+  "type" : "decrement"
+}
+
+
+// our reducer
+const do_increment = (state) => {
+  return state + 1
+}
+
+const do_decrement = (state) => {
+  return state - 1
+}
+
+
+//store to bring it all together
+
+function counter_store(state = init_state, action) {
+  switch (action.type) {
+    case 'increment':
+      return do_increment(state)
+    case 'decrement':
+      return do_decrement(state)
+    default:
+      return state
+  }
+}
+
+
+// now we actuall use redux api's
+let store = createStore(counter_store)
+
+store.subscribe( () => {
+  // subscribe to any changes in state
+  console.log("change happend in state")
+  console.log(store.getState())
+})
+
+store.dispatch(increment_action)  //1
+
+store.dispatch(decrement_action) // 0
+
+store.dispatch(increment_action) // 1
+
+store.dispatch(increment_action) // 2
+
+
+
+let input = {
+  "count" : 1
+}
+
+function dummyReducer(input){
+  return {
+    ...input,
+    "count" : input.count + 1
+  }
+}
+
+console.log(dummyReducer(input))
+
+
+
+
 
 
 
